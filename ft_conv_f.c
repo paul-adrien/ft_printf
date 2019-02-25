616e7ac41651d1d41d132bed9ce43cdf07a1b5fb
Paul-adrien LAURENT <plaurent@e1r10p23.42.fr>
2019-02-25 10:28:15 +0100

    ajout %f

---
+++ ft_conv_f.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_conv_f.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plaurent <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/14 17:43:02 by plaurent          #+#    #+#             */
+/*   Updated: 2019/02/14 20:26:12 by plaurent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static long	ft_power(long nb, int power)
+{
+	if (power > 1)
+		return (nb * ft_recursive_power(nb, (power - 1)));
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb);
+}
+
+static int	st_countsize(int n)
+{
+	int i;
+
+	i = 0;
+	if (n <= 0)
+		i++;
+	while (n != 0)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
+
+static char		*ft_ftoa(double n, int p)
+{
+	int		i;
+	int		j;
+	long	k;
+	char	*str;
+
+	j = 0;
+	k = 1;
+	i = st_countsize(n * ft_recursive_power(10, p)) + 1;
+	if (!(str = ft_strnew(i)))
+		return (NULL);
+	if (n < 0)
+	{
+		n = -n;
+		str[0] = '-';
+	}
+	if (n == 0)
+	{
+		str[0] = '0';
+		str[1] = '.';
+		while (j < p)
+			str[j++ + 2] = '0';
+		return (str);
+	}
+	else
+		k = n * ft_power(10, p);
+	while (k != 0)
+	{
+		str[i - 1] = (k % 10) + '0';
+		k = k / 10;
+		i--;
+		j++;
+		if (j == p)
+			str[i-- - 1] = '.';
+	}
+	if (k == 0)
+	{
+		ft_putnbr(p);
+		ft_putchar('k');
+		k = k*100;
+		ft_putnbr(k);
+	}
+	return (str);
+}
+
+char	*ft_conv_f(t_asset asset, va_list ap)
+{
+	char	*str;
+	char	*str2;
+	int		i;
+
+	i = asset.precision;
+	if (asset.precision == -1)
+		i = 6;
+	if (asset.precision == 0)
+		i = 1;
+	/*if (asset.precision >= 10)
+	{
+		str = ft_ftoa(va_arg(ap, double), 9);
+		str2 = ft_strnew(st_countsize(ft_strlen(str)));
+	}
+	else*/
+		str = ft_ftoa(va_arg(ap, double), i);
+	return (str);
+}
